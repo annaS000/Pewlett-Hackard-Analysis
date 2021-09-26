1c0003d23a281866fa7c3f3b9a95f3d6b122307b
anna sand <sand7anna@outlook.com>
2021-09-26 19:11:19 -0400

    updating

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,4 @@
---getting names and emp_no
+--Create a Retirement Titles table
 SELECT e.emp_no, 
     e.first_name, 
     e.last_name,
@@ -12,7 +12,7 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no
 
--- Use Dictinct with Orderby to remove duplicate rows
+--create a Unique Titles table
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
 last_name,
@@ -22,14 +22,14 @@ FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
 
---getting number of retiring titles
+--Create a Retiring Titles table
 SELECT COUNT(title),title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 
-
+--Create a Mentorship Eligibility table
 SELECT DISTINCT ON (emp_no) e.emp_no, 
                             e.first_name, 
                             e.last_name,
